457d27807e3abd6dba43cfa163c0ebbf571e8cdd
Subhendu Sarkar <64896535+subhendu20@users.noreply.github.com>
2023-07-30 12:02:36 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -119,7 +119,28 @@ function App() {
 
 
 
+  let vh = window.innerHeight * 0.01;
+  
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+
+
+
+
+
+
+
+
+
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+  },[])
 
   useEffect(() => {
     checklog()
